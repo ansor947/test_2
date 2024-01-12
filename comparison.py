98a2492dfdb14cca0b9ed4c5d254938a450f0d91
ansor947 <ansorpy@gmail.com>
2024-01-12 07:18:27 +0300

    first_1

--- comparison.py
+++ comparison.py
@@ -3,20 +3,20 @@ import operator
 
 
 def compare(string: str) -> str:
-    pattern = r"(\W?\d+)(\W)(\W?\d+)"
-
+    pattern = r"(\W?\d+)(\W)(\W?\d+)" 
     string_reg = re.split(pattern, string)
     for element in string_reg:
         if element == '':
             string_reg.remove(element)
             if string_reg[1] == '<':
-                result = operator.lt(string_reg[0], string_reg[2])
+                result = operator.lt(int(string_reg[0]), int(string_reg[2]))
             elif string_reg[1] == '>':
-                result = operator.gt(string_reg[0], string_reg[2])
+                result = operator.gt(int(string_reg[0]), int(string_reg[2]))
+
     return result
 
 
-# print(compare('548>787'))
+# print(compare('112>12'))
 # print(compare('-758<-978'))
 # print(compare('758>-978'))
 # print(compare('758<-978'))
